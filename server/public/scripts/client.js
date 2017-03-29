f20cc4dd41070dbf0a53312112fae7fe2718c8da
Dan Zera <dczera@gmail.com>
2017-03-29 16:14:51 -0500

    add basic structure to client.js and begin filling in

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
   console.log("jquery sourced");
 });
+
+// INITIALIZE THE DOCUMENT
+function init() {
+  addEventListeners();
+}
+
+// EVENT LISTENERS
+function addEventListeners() {
+  // generate button
+  $('#inputDiv').on('click', '#generate', function() {
+
+  });
+  // old teams button
+  $('#inputDiv').on('click', '#showOld', getHistoricalTeams);
+}
+// DOM FUNCTIONS
+// display newly generated teams
+
+// display historical teams
+
+// AJAX CALLS
+// get newly generated teams
+
+// get historical teams
+function getHistoricalTeams() {
+  $.ajax({
+    type: 'GET',
+    url: '/old',
+    success: function(res) {
+      console.log('server response on ')
+    }
+  })
+}
