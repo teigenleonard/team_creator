0fa4d4509d1fd0fb425434a02884e109cc537e2f
Dan Zera <dczera@gmail.com>
2017-03-29 16:19:10 -0500

    complete event listeners and ajax call code in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  console.log("jquery sourced");
+  init();
 });
 
 // INITIALIZE THE DOCUMENT
@@ -10,19 +10,33 @@ function init() {
 // EVENT LISTENERS
 function addEventListeners() {
   // generate button
-  $('#inputDiv').on('click', '#generate', function() {
-
-  });
+  $('#inputDiv').on('click', '#generate', getNewTeams);
   // old teams button
   $('#inputDiv').on('click', '#showOld', getHistoricalTeams);
 }
 // DOM FUNCTIONS
 // display newly generated teams
+function displayNewTeams(teamsArray) {
+
+}
 
 // display historical teams
+function displayHistoricalTeams(teamsArray) {
+
+}
 
 // AJAX CALLS
 // get newly generated teams
+function getNewTeams() {
+  $.ajax({
+    type: 'GET',
+    url: '/new',
+    success: function(res) {
+      console.log('server response on /new route:', res);
+      displayNewTeams(res);
+    }
+  });
+}
 
 // get historical teams
 function getHistoricalTeams() {
@@ -30,7 +44,8 @@ function getHistoricalTeams() {
     type: 'GET',
     url: '/old',
     success: function(res) {
-      console.log('server response on ')
+      console.log('server response on /old route:', res);
+      displayHistoricalTeams(res);
     }
-  })
+  });
 }
