f50f569b719b3ffb27af23a26c9ac071f5643259
Teigen Leonard <teigenl@gmail.com>
2017-03-29 22:59:58 -0500

    add calls to teams.js

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -3,10 +3,43 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 
-var TeamsSchema = mongoose.Schema({
-  team : Array
+var TeamSchema = mongoose.Schema({
+  teamnumber: Number,
+  first : String,
+  last: String,
+  date: Date
 });
 
-var teams = mongoose.model('teams', TeamsSchema);
+var Teams = mongoose.model('teams', TeamsSchema);
+
+router.get("/", function(req, res) {
+    teams.find(function(err, allTeams) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        res.send(allTeams);
+    });
+});
+
+
+router.post("/", function(req, res) {
+
+    var team = new Teams();
+    team.teamnumber = req.body.teamnumber;
+    team.first = req.body.first;
+    team.last = req.body.last;
+    team.date = req.body.date;
+    team.save(function(err, savedTeam) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+
+        res.send(savedTeam);
+    });
+});
+
+
 
 module.exports = router;
