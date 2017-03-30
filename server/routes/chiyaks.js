3aa83ee4b7d8f6d633e0b5d4f1c752af6fa7078e
Teigen Leonard <teigenl@gmail.com>
2017-03-29 22:28:37 -0500

    beautify code

--- server/routes/chiyaks.js
+++ server/routes/chiyaks.js
@@ -4,38 +4,38 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 var ChiyaksSchema = mongoose.Schema({
-  first : String,
-  last : String
+    first: String,
+    last: String
 });
 
 
 var Chiyaks = mongoose.model('chiyaks', ChiyaksSchema);
 
 
-router.get("/", function(req,res){
-  chiyaks.find(function(err, allChiyaks){
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }
-    res.send(allChiyaks);
-  });
+router.get("/", function(req, res) {
+    chiyaks.find(function(err, allChiyaks) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        res.send(allChiyaks);
+    });
 });
 
 
-router.post("/", function(req,res){
+router.post("/", function(req, res) {
 
-  var chiyak = new Chiyaks();
-  chiyak.first = req.body.first;
-  chiyak.last = req.body.last;
-  employee.save(function(err, savedChiyak){
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }
+    var chiyak = new Chiyaks();
+    chiyak.first = req.body.first;
+    chiyak.last = req.body.last;
+    employee.save(function(err, savedChiyak) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
 
-    res.send(savedChiyak);
-  });
+        res.send(savedChiyak);
+    });
 });
 
 module.exports = router;
