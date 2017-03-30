7564ac0152cb166636aeb9ffdb552a3060a068c7
Dan Zera <dczera@gmail.com>
2017-03-29 22:51:00 -0500

    finish DOM update function - combined historical team and new team displays into one function to DRY out the code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,10 +7,10 @@ var testArray = [team1, team2, team3, team4];
 var historicalTest = [team1, team2, team3, team4, team4, team3, team2, team1, team2, team3, team4, team1, team3, team2, team1, team4];
 
 $(document).ready(function() {
-  // REMOVE THIS FUNCTION CALL WHEN READY TO TEST FULL FUNCTIONALITY
+  // REMOVE THE NEXT 2 FUNCTION CALLS WHEN READY TO TEST FULL FUNCTIONALITY
   // JUST USING THIS TO TEST THE REST OF CLIENT.JS FUNCTIONALITY
-  displayHistoricalTeams(historicalTest);
-  // displayNewTeams(testArray);
+  //displayTeams(historicalTest, true);
+  displayTeams(testArray, false);
   // UNCOMMENT init() WHEN READY TO TEST FULL FUNCTIONALITY
   // init();
 });
@@ -27,52 +27,15 @@ function addEventListeners() {
   // old teams button
   $('#showHistory').on('click', getHistoricalTeams);
 }
+
 // DOM FUNCTIONS
-// display newly generated teams
-function displayNewTeams(teamsArray) {
+// display array of teams
+function displayTeams(teamsArray, isHistoricalData) {
+  // decide on heading text based on which set of data we're receiving
+  var heading = isHistoricalData ? (heading = 'Historical Teams') : (heading = 'New Teams');
   $el = $('#outputDiv');
-  $el.empty();
-  $el.append('<h1>New Teams</h1>');
-  displayTeams($el, teamsArray);
-  // for (var i = 0; i < teamsArray.length; i++) {
-  //   var teamNumber = i + 1; // 1-index team numbers
-  //   var team = teamsArray[i]; // pull individual team out of teamsArray
-  //   // each team will be displayed in its own table
-  //   $el.append('<div class="col-xs-3"><table class="table"></table></div>');
-  //   var $table = $el.children().last().children().first();
-  //   // table header displays "Team #"
-  //   $table.append('<thead><tr><th>Team ' + teamNumber + '</th></tr></thead><tbody></tbody>');
-  //   // each team member is displayed on their own row
-  //   team.forEach(function(person) {
-  //     var name = person.first + ' ' + person.last;
-  //     $table.append('<tr><td>' + name + '</tr></td>');
-  //   });
-  // } // end loop through teamsArray
-} // end displayNewTeams()
-
-// display historical teams
-function displayHistoricalTeams(teamsArray) {
-  $el = $('#outputDiv');
-  $el.empty();
-  $el.append('<h1>Historical Teams</h1>');
-  displayTeams($el, teamsArray);
-  // for (var i = 0; i < teamsArray.length; i++) {
-  //   var teamNumber = i + 1; // 1-index team numbers
-  //   var team = teamsArray[i]; // pull individual team out of teamsArray
-  //   // each team will be displayed in its own table
-  //   $el.append('<div class="col-xs-3"><table class="table"></table></div>');
-  //   var $table = $el.children().last().children().first();
-  //   // table header displays "Team #"
-  //   $table.append('<thead><tr><th>Team ' + teamNumber + '</th></tr></thead><tbody></tbody>');
-  //   // each team member is displayed on their own row
-  //   team.forEach(function(person) {
-  //     var name = person.first + ' ' + person.last;
-  //     $table.append('<tr><td>' + name + '</tr></td>');
-  //   });
-  // } // end loop through teamsArray
-}
-
-function displayTeams($el, teamsArray) {
+  $el.empty(); // empty the outputDiv
+  $el.append('<h1>' + heading + '</h1>'); // display the appropriate header
   for (var i = 0; i < teamsArray.length; i++) {
     var teamNumber = i + 1; // 1-index team numbers
     var team = teamsArray[i]; // pull individual team out of teamsArray
@@ -99,7 +62,7 @@ function getNewTeams() {
       console.log('server response on /new route:', res);
       // dispaly new teams on the DOM
       // res = [[{Team1-Member1}, {T1-M2}, {T1-M3}], [{Team2-Member1}, {T2-M2}, {T2-M3}], [team3], [team4]];
-      displayNewTeams(res);
+      displayTeams(res, false);
     } // end success
   }); // end ajax
 } // end getNewTeams()
@@ -112,7 +75,7 @@ function getHistoricalTeams() {
     success: function(res) {
       console.log('server response on /old route:', res);
       // display old teams on the DOM
-      displayHistoricalTeams(res);
+      displayTeams(res, true);
     } // end success
   }); // end ajax
 } // end getHistoricalTeams()
